ce891be1ecbb8f293d88706d7e7a91ed235d5c03
magur0don <magurodon.10000yen@gmail.com>
2024-07-25 15:20:18 +0900

    1つ目のパズルまで

---
+++ Assets/Scripts/ReturnObject.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReturnObject : MonoBehaviour
+{
+    // ①OnTrigerEnterを使って侵入判定を取得する。
+    private void OnTriggerEnter(Collider other)
+    {
+        // ②侵入してきたGameObjectのPosition,Rotationを、自分のPosition,Rotationで代入する
+        other.gameObject.transform.position = this.gameObject.transform.position;
+        other.gameObject.transform.rotation = this.gameObject.transform.rotation;
+        // ③侵入してきたGameObjectのRigidBodyを取得し、
+        // useGravityをオフにして、IsKinematicをオンにする。
+        var otherRigidbody = other.gameObject.GetComponent<Rigidbody>();
+        otherRigidbody.useGravity = false;
+        otherRigidbody.isKinematic = true;
+
+        // ④自分のMeshRendererのenabledをオフにする
+        this.gameObject.GetComponent<MeshRenderer>().enabled = false;
+    }
+
+
+}
